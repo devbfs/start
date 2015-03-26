89a1a8750a8e5d94c2ece5f49e3648b6064cf6cc
Jay Wagner <jwagner@backflipstudios.com>
2015-03-26 17:36:30 -0600

    Creating .git-credentials file and adding PEP-8 quoting.

--- panda.py
+++ panda.py
@@ -9,41 +9,41 @@ from os.path import expanduser
 from socket import gethostname
 
 agent_support = {
-    "brew": [
-        "androidndk-9c-android",
-        "androidndk-9d-android",
-        "androidndk-10d-android",
-        "xcode-6.1.1-mac",
-        "xcode-6.2-mac",
-        "unity-4.6.3f1-mac",
-        "unity-4.6.3p3-mac",
-        "unity-5.0.0b18-mac"
+    'brew': [
+        'androidndk-9c-android',
+        'androidndk-9d-android',
+        'androidndk-10d-android',
+        'xcode-6.1.1-mac',
+        'xcode-6.2-mac',
+        'unity-4.6.3f1-mac',
+        'unity-4.6.3p3-mac',
+        'unity-5.0.0b18-mac'
     ],
-    "gem": [
-        "xcodeproj -v 0.19.2"
+    'gem': [
+        'xcodeproj -v 0.19.2'
     ],
-    "pip": [
-        ["paramiko",
-         "requests"
+    'pip': [
+        ['paramiko',
+         'requests'
         ]
     ]
 }
 
 bamboo_support = {
-    "brew": [
+    'brew': [
     ],
-    "gem": [
+    'gem': [
     ],
-    "pip": [
+    'pip': [
     ]
 }
 
 web_support = {
-    "brew": [
+    'brew': [
     ],
-    "gem": [
+    'gem': [
     ],
-    "pip": [
+    'pip': [
     ]
 }
 
@@ -51,6 +51,10 @@ gitconfig = '''
 [credential "https://backflipstudios.kilnhg.com"]
 username = {}
 helper = osxkeychain
+'''
+
+gitcredentials = '''
+https://{}:{}@backflipstudios.kilnhg.com
 '''
 
 hgconfig = '''
@@ -146,43 +150,43 @@ def call(args):
 
 def ask_or_exit(ret):
     while True:
-        choice = raw_input("Continue? (y/n): ")
-        if choice == "y" or choice == "Y":
+        choice = raw_input('Continue? (y/n): ')
+        if choice == 'y' or choice == 'Y':
             break
-        elif choice == "n" or choice == "N":
-            print("Aborting.")
+        elif choice == 'n' or choice == 'N':
+            print('Aborting.')
             sys.exit(ret)
 
 
-def install_call(args, fail_on_error):
+def install_call(args, fail_on_error, quiet=False):
     ret = call(args)
     if ret != 0:
-        print("\nERROR: Failed to install package: {}".format(args))
+        print('\nERROR: Failed to install package: {}'.format(args))
         if fail_on_error:
             sys.exit(ret)
-        else:
+        elif not quiet:
             ask_or_exit(ret)
     
 
 def brew_install(package_name, fail_on_error):
-    install_call(["brew", "install"] + package_name.split(), fail_on_error)
+    install_call(['brew', 'install'] + package_name.split(), fail_on_error)
     
 
 def pip_install(package_props, fail_on_error):
-    install_call(["pip", "install"] + package_props, fail_on_error)
+    install_call(['pip', 'install'] + package_props, fail_on_error)
     
 
 def gem_install(package_name, fail_on_error):
-    install_call(["gem", "install"] + package_name.split(), fail_on_error)
+    install_call(['gem', 'install'] + package_name.split(), fail_on_error)
 
 
 def write_config(path, content):
-    with open(expanduser(path), "w") as f:
+    with open(expanduser(path), 'w') as f:
         f.write(content)
 
 
 def write_profile_config():
-    write_config("~/.profile", profileconfig.format(expanduser("~")))
+    write_config('~/.profile', profileconfig.format(expanduser('~')))
 
 
 def write_kiln_config(config_parser):
@@ -196,32 +200,35 @@ def write_kiln_config(config_parser):
             print 'ERROR: Hostname does not match any values in .tokens.'
             sys.exit(1)
     else:
-        kiln_access_token = raw_input("Kiln Access Token: ")
+        kiln_access_token = raw_input('Kiln Access Token: ')
 
     if kiln_access_token is not None and len(kiln_access_token) > 0:
-        write_config("~/.hgrc", hgconfig.format(kiln_access_token))
-        write_config("~/.gitconfig", gitconfig.format(kiln_access_token))
+        write_config('~/.hgrc', hgconfig.format(kiln_access_token))
+        write_config('~/.gitconfig', gitconfig.format(kiln_access_token))
+        write_config('~/.git-credentials', gitcredentials.format(kiln_access_token, 'apw'))
 
 
 def write_github_config(config_parser):
     if config_parser:
         github_access_token = config_parser.get('github', 'token')
     else:
-        github_access_token = raw_input("Github Access Token: ")
+        github_access_token = raw_input('Github Access Token: ')
 
     if github_access_token is not None and len(github_access_token) > 0:
-        write_config("~/.backflipbrew", backflipbrewconfig.format(github_access_token))
+        write_config('~/.backflipbrew', backflipbrewconfig.format(github_access_token))
    
 
 def main():
-    parser = argparse.ArgumentParser(description="Panda Build System setup script.")
-    parser.add_argument("-e", "--emacs", help="Install/configure basic emacs setup", action="store_true", required=False)
-    parser.add_argument("-a", "--agent", help="Install Agent (red-panda) support", action="store_true", required=False)
-    parser.add_argument("-b", "--bamboo", help="Install Bamboo (panda) support", action="store_true", required=False)
-    parser.add_argument("-w", "--web", help="Install Web (kungfu) support", action="store_true", required=False)
-    parser.add_argument("-i", "--environment", help="Configure basic environment profile", action="store_true", required=False)
-    parser.add_argument("-k", "--kiln", help="Configure git and mercurial to use Kiln Access Tokens", action="store_true", required=False)
-    parser.add_argument("-g", "--github", help="Configure to use Github Access Tokens for Homebrew", action="store_true", required=False)
+    parser = argparse.ArgumentParser(description='Panda Build System setup script.')
+    parser.add_argument('-e', '--emacs', help='Install/configure basic emacs setup', action='store_true', required=False)
+    parser.add_argument('-a', '--agent', help='Install Agent (red-panda) support', action='store_true', required=False)
+    parser.add_argument('-b', '--bamboo', help='Install Bamboo (panda) support', action='store_true', required=False)
+    parser.add_argument('-w', '--web', help='Install Web (kungfu) support', action='store_true', required=False)
+    parser.add_argument('-i', '--environment', help='Configure basic environment profile', action='store_true', required=False)
+    parser.add_argument('-k', '--kiln', help='Configure git and mercurial to use Kiln Access Tokens', action='store_true', required=False)
+    parser.add_argument('-g', '--github', help='Configure to use Github Access Tokens for Homebrew', action='store_true', required=False)
+    parser.add_argument('-q', '--quiet', help='Quiet mode. Suppresses error messages from most failed installations.',
+                        action='store_true', required=False)
     args = parser.parse_args()
 
     config = ConfigParser.SafeConfigParser()
@@ -236,21 +243,21 @@ def main():
         return 1
 
     if args.environment:
-        print("Installing basic environment profile...")
+        print('Installing basic environment profile...')
         write_profile_config()
 
     if args.kiln:
-        print("Setting up SCM for Kiln...")
+        print('Setting up SCM for Kiln...')
         write_kiln_config(config)
 
     if args.github:
-        print("Setting Github for Homebrew...")
+        print('Setting Github for Homebrew...')
         write_github_config(config)
 
     if args.emacs:
-        print("Installing basic emacs setup...")
-        brew_install("emacs", False)
-        write_config("~/.emacs", emacsconfig)
+        print('Installing basic emacs setup...')
+        brew_install('emacs', False)
+        write_config('~/.emacs', emacsconfig)
 
     if args.agent:
         print('Cloning the panda repository...')
@@ -261,36 +268,36 @@ def main():
         install_call(['git', 'checkout', 'agent'], False)
         chdir(current_dir)
 
-        print("Installing Xcode support...")
-        for brew_package in agent_support["brew"]:
+        print('Installing Xcode support...')
+        for brew_package in agent_support['brew']:
             brew_install(brew_package, False)
 
-        for pip_package in agent_support["pip"]:
+        for pip_package in agent_support['pip']:
             pip_install(pip_package, False)
 
-        for gem_package in agent_support["gem"]:
+        for gem_package in agent_support['gem']:
             gem_install(gem_package, False)
 
     if args.bamboo:
-        print("Installing Xcode support...")
-        for brew_package in bamboo_support["brew"]:
+        print('Installing Xcode support...')
+        for brew_package in bamboo_support['brew']:
             brew_install(brew_package, False)
 
-        for pip_package in bamboo_support["pip"]:
+        for pip_package in bamboo_support['pip']:
             pip_install(pip_package, False)
 
-        for gem_package in bamboo_support["gem"]:
+        for gem_package in bamboo_support['gem']:
             gem_install(gem_package, False)
 
     if args.web:
-        print("Installing Xcode support...")
-        for brew_package in web_support["brew"]:
+        print('Installing Xcode support...')
+        for brew_package in web_support['brew']:
             brew_install(brew_package, False)
 
-        for pip_package in web_support["pip"]:
+        for pip_package in web_support['pip']:
             pip_install(pip_package, False)
 
-        for gem_package in web_support["gem"]:
+        for gem_package in web_support['gem']:
             gem_install(gem_package, False)
 
     return 0
